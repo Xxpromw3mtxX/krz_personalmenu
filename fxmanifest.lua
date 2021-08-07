e4bb59a6b8c3390a81ddeb2a592290ee834d3313
Francesco Metta <fra.metta@hotmail.it>
2021-08-07 19:39:08 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,21 @@
-fx_version 'cerulean'
-game 'gta5'
-
-lua54 'yes'
+fx_version 'adamant'
+games {'gta5'}
 
 author 'Korioz'
 description 'PersonalMenu for FiveM developed on top of ESX and RageUI'
-version '2.1'
+version '2.0'
 
 dependency 'es_extended'
 
-shared_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'init.lua'
-}
-
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fr.lua',
+	'config.lua',
 	'server/main.lua'
 }
 
@@ -31,8 +29,17 @@ client_scripts {
 	"dependencies/menu/MenuController.lua",
 
 	"dependencies/menu/elements/*.lua",
-	"dependencies/menu/items/*.lua",
+	"dependencies/menu/items/*.lua"
+}
 
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fr.lua',
+	'config.lua',
 	'client/main.lua',
 	'client/other.lua'
 }
